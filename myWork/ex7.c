263902acd1f875c9e7f1894a35326172c5524301
zaighumrajput <zaighum47@gmail.com>
2016-04-18 15:21:52 +0000

    some breaking

--- myWork/ex7.c
+++ myWork/ex7.c
@@ -9,11 +9,13 @@ int main(int argv, char *argc[])
 	char first_name[] = "Zed";
 	char last_name[] = "Shaw";
 	
+	first_name[4] = 1;
+	
 	printf("You are %d miles away.\n", distance);
 	printf("You have %f levels of power.\n", power);
 	printf("You have %f awesome super powers.\n", super_power);
 	printf("I have an initial %c.\n", initial);
-	printf("I have a first name %s.\n", first_name+100);
+	printf("I have a first name %s.\n", first_name);
 	printf("I have a last name %s.\n", last_name);
 	printf("My whole name is %s %c. %s.\n", first_name, initial, last_name);
 
