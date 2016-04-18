5d447b8e10040524ebc6ead272ef0d5bc7ac0de5
zaighumrajput <zaighum47@gmail.com>
2016-04-18 04:12:11 +0000

    variables, printf

---
+++ myWork/ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argv, char *argc[])
+{
+	int age;
+	int height = 75;
+
+	printf("I am %d years old.\n",argc);
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
+}
+
