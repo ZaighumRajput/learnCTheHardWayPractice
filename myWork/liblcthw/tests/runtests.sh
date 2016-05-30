46200972b8129505e433b910f9fb5452d8ada9fe
zaighumrajput <zaighum47@gmail.com>
2016-05-30 22:43:03 +0000

    mostly working

--- myWork/liblcthw/tests/runtests.sh
+++ myWork/liblcthw/tests/runtests.sh
@@ -2,14 +2,14 @@ echo "Running unit tests:"
 
 for i in tests/*_tests
 do
-	$i 2>&1 >> tests/tests.log
-	if [ ! $? ]
-	then
-		echo "ERROR in test $i: here's tests/tests.log"
-		echo "--------"
-		tail tests/tests.log
-		exit 1
-	fi
+    $i 2>&1 >> tests/tests.log
+    if [ ! $? ]
+    then
+        echo "ERROR in test $i: here's tests/tests.log"
+        echo "------"
+        tail tests/tests.log
+        exit 1
+    fi
 done
 
 echo ""
